56e133db27800c7db5b51912b7627f8a93927ae9
Valeri Gladunik <Valeri.Gladunik@tptlive.ee>
2025-10-27 11:40:43 +0200

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+    content: [
+        './src/**/*.{html,njk,js}',
+    ],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import vituum from 'vituum';
 import nunjucks from '@vituum/vite-plugin-nunjucks';
+import tailwindcss from '@vituum/vite-plugin-tailwindcss';
 
 export default {
     plugins: [
+        tailwindcss(),
         vituum(),
         nunjucks({
             root: 'src',
